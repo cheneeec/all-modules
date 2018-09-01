7cf0dfc7cc954395f6097ebce830181355fa2c47
chenlei <332472988@qq.com>
2018-09-01 15:42:30 +0800

    update...

--- j-video/src/test/java/com/earnest/video/parse/IQiYiParse.java
+++ j-video/src/test/java/com/earnest/video/parse/IQiYiParse.java
@@ -11,21 +11,24 @@ import java.io.IOException;
 import java.util.Date;
 
 public class IQiYiParse {
+
     private HttpClient httpClient = HttpClients.custom().setUserAgent(Browser.IPHONE_X.userAgent()).build();
     private BasicResponseHandler basicResponseHandler = new BasicResponseHandler();
 
     @Test
     public void t() throws IOException {
+        //http://www.iqiyi.com/v_19rqzi2kuo.html
         //1.取得id=1259275400和web_id=1259275400
         String execute = httpClient.execute(new HttpGet("http://jiexi.071811.cc/jx2.php?url=http://www.iqiyi.com/v_19rqzi1f7s.html"), basicResponseHandler);
         System.out.println(execute);
+       //2.
+
 
     }
 
     @Test
-    public void time(){
-        //1535644800
-        double v = 157248E5 + new Date().getTime();
-        System.out.println(v);
+    public void time() throws IOException {
+
+        System.out.println(httpClient.execute(new HttpGet("http://jiexi.071811.cc/stapi.php?url=http://www.iqiyi.com/v_19rqzi1f7s.html"), basicResponseHandler));
     }
 }
