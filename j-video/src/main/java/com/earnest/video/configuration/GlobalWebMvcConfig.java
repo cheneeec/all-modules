0c3d040dbc2760235b5355126ace1799dfd0282c
earnest <332472988@qq.com>
2018-10-04 11:45:23 +0800

    update...

--- j-video/src/main/java/com/earnest/video/configuration/GlobalWebMvcConfig.java
+++ j-video/src/main/java/com/earnest/video/configuration/GlobalWebMvcConfig.java
@@ -6,14 +6,13 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-public class GlobalWebMvcConfig extends WebMvcConfigurerAdapter {
+public class GlobalWebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
--- j-video/src/main/java/com/earnest/video/service/CachedVideoService.java
+++ j-video/src/main/java/com/earnest/video/service/CachedVideoService.java
@@ -10,13 +10,16 @@ import org.springframework.util.CollectionUtils;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
-public  class CachedVideoService<T extends BaseVideoEntity> implements BasicQueryAndPersistenceVideoService<T> {
+public class CachedVideoService<T extends BaseVideoEntity> implements BasicQueryAndPersistenceVideoService<T> {
 
     private final Map<Long, T> videoMap;
 
     private final Map<BaseVideoEntity.Category, List<T>> categoryTMap;
+
+    private final AtomicLong idLong = new AtomicLong(1000);
 
     public CachedVideoService(Map<Long, T> videoMap) {
         Assert.notNull(videoMap, "the videoMap is required");
@@ -31,21 +34,46 @@ public  class CachedVideoService<T extends BaseVideoEntity> implements BasicQuer
 
     @Override
     public void save(List<T> entities) {
+
         if (!CollectionUtils.isEmpty(entities)) {
-            categoryTMap.putAll(entities.stream()
+            //将结果按照id添加
+            Map<BaseVideoEntity.Category, List<T>> videosByCategory = entities.stream()
                     .filter(Objects::nonNull)
+                    .peek(e -> {//当没有ID时为其设置ID
+                        if (e.getId() == null) {
+                            e.setId(idLong.incrementAndGet());
+                        }
+                    })
                     .peek(e -> videoMap.put(e.getId(), e))
-                    .collect(Collectors.groupingBy(BaseVideoEntity::getCategory)));
+                    .collect(Collectors.groupingBy(BaseVideoEntity::getCategory));
+
+            //将结果进行分类添加
+            videosByCategory.keySet()
+                    .forEach(c ->
+                            categoryTMap.compute(c, ((category, ts) -> {
+                                ts = Optional.ofNullable(ts).orElse(new ArrayList<>());
+                                ts.addAll(videosByCategory.get(category));
+                                return ts;
+                            }))
+                    );
+
         }
     }
 
     @Override
     public void save(T entity) {
         if (entity != null) {
+            if (entity.getId() == null) {
+                entity.setId(idLong.incrementAndGet());
+            }
+
             videoMap.put(entity.getId(), entity);
-            List<T> entities = new ArrayList<>();
-            entities = categoryTMap.getOrDefault(entity.getCategory(), entities);
-            entities.add(entity);
+
+            categoryTMap.compute(entity.getCategory(), ((category, ts) -> {
+                ts = Optional.ofNullable(ts).orElse(new ArrayList<>());
+                ts.add(entity);
+                return ts;
+            }));
 
         }
 
@@ -77,12 +105,18 @@ public  class CachedVideoService<T extends BaseVideoEntity> implements BasicQuer
     @Override
     public Page<T> findByCategory(Pageable pageRequest, BaseVideoEntity.Category category) {
         Assert.notNull(pageRequest, "pageRequest is required");
+        List<T> content = categoryTMap.get(category);
+        if (content == null) {
+            return Page.empty();
+        }
+
         return new PageImpl<>(
                 //获得内容
-                categoryTMap.get(category).stream().skip(pageRequest.getOffset())
+                content.stream().skip(pageRequest.getOffset())
                         .limit(pageRequest.getPageSize())
                         .collect(Collectors.toList()),
                 pageRequest, videoMap.size());
 
     }
+
 }
